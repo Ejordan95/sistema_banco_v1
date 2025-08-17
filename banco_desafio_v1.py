48f0a81228e68c13988d55f12e9b5d140924123c
Ejordan95 <100079442+Ejordan95@users.noreply.github.com>
2025-08-17 15:13:48 -0300

    Desavio sistema bancario v1

---
+++ banco_desafio_v1.py
@@ -0,0 +1,67 @@
+saldo = 0
+limite_diario = 500
+LIMITE_SAQUES = 3
+registro_saque = 0
+extrato = ""
+
+menu = '''
+                Bem vindo ao Banco XPTO
+
+Em que posso ajudar?
+[1] Deposito
+[2] Saque
+[3] Extrato
+[0] Sair
+
+
+'''
+while True:
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe valor para Deposito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposito: R$ {valor:.2f}\n"
+        
+        else:
+            print("Erro".center(10,"!"))
+            print("Informe um valor válido.")
+    
+    elif opcao == "2":
+        valor = float(input("Informa o valor para Saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite_diario
+        excedeu_saque = registro_saque >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor de saque excede o limite.")
+
+        elif excedeu_saque:
+            print("Operação falhou! Número máximo de saques excedido")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            registro_saque += 1
+        else:
+            print("Operação Falhou! O valor informado é inválido.")
+    
+    elif opcao == "3":
+        print(" EXTRATO ".center(20,"#"))
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("####################")
+    
+    elif opcao == "0":
+        break
+
+    else:
+        print("Operação inválida!")
+
+#print(menu)
